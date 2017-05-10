b6653645780b8fb65b7eab4b190234c2daec96d9
Murad Mikayilzadeh <m.murad@code.edu.az>
2017-05-10 23:20:02 +0400

    5/201/2017 23:20

--- mini.js
+++ mini.js
@@ -1,12 +1,29 @@
-// JS library for mini 2D games
+/**
 
-// Murad Mikayilzada
+*Mini.js
+
+*Murad Mikayilzada
+
+*Javascript library for mini 2D games
+
+*Created at 5.10.2017
+
+*/
 
 
 var Game = (function () {
 
+	
+    /**
+    
+    @description Oyun penceresi yaradir ve "Ishleyir" mesaji verir.
 
-	//Oyun penceresi yaradir ve "Ishleyir" mesaji verir
+    @param {number,number}
+
+    @returns void
+    
+    */
+
     function Game(width,height) {
 
         console.log("It's working");
@@ -24,7 +41,15 @@ var Game = (function () {
     }
 
 
-    //Bu funksiya oyun penceresinin bg-sini deyisir
+    /**
+    
+    @description Bu funksiya oyun penceresinin bg-sini deyisir
+
+    @param {string}
+
+    @returns void
+    
+    */
     Game.prototype.changeWindowBg = function(color){
 
     	this.color = color;
@@ -33,7 +58,16 @@ var Game = (function () {
 
     }
 
-    //Bu funksiya oyun penceresine dordbucaqli cekir
+
+    /**
+    
+    @description Bu funksiya oyun penceresine dordbucaqli cekir
+
+    @param {number,number,number,number,string}
+
+    @returns void
+    
+    */
     Game.prototype.drawRect = function(a,b,c,d,bg){
 
         this.a = a;
@@ -50,17 +84,33 @@ var Game = (function () {
 
     }
 
-    //Bu funksiya ile koordinatlari array seklinde vermekle istenilen fiquru cekmek olar
+    //
 
-    //Meselen, koordinatlar adinda array yaradilir ve asagida oldugu kimi ona deyerler oturulur.a
+     /**
+    
+    @description Bu funksiya ile koordinatlari array seklinde vermekle ve rengi daxil etmekle istenilen fiquru cekmek olar
 
-    // var koordinatlar = [[[0,0],[100,100]],[[100,100],[400,300]]] -------- koordinatlar[0] ve koordinatlar[1] heresi ayri xetti temsil edir.
-    //Burada istenilen qeder duz xett koordinatlari gonderile biler. koordinatlar[0][0] xettin baslangici, koordinatlar[0][1] ise xettin son noqtesinin
-    //koordinatlaridir. Hemin qayda koordinatlar[1] icerisindeki koordinatlar ucun de kecerlidir.
+    Meselen, koordinatlar adinda array yaradilir ve asagida oldugu kimi ona deyerler oturulur.a
 
-    Game.prototype.drawFigure = function(coordinats){
+    var koordinatlar = [[[0,0],[100,100]],[[100,100],[400,300]]] -------- koordinatlar[0] ve koordinatlar[1] heresi ayri xetti temsil edir.
+    Burada istenilen qeder duz xett koordinatlari gonderile biler. koordinatlar[0][0] xettin baslangici, koordinatlar[0][1] ise xettin son noqtesinin
+    koordinatlaridir. Hemin qayda koordinatlar[1] icerisindeki koordinatlar ucun de kecerlidir.
+
+    Funksiya bu cur istifade edilir.
+
+    oyun.drawFigure(koordinatlar,reng);
+
+    @param {array,string}
+
+    @returns void
+    
+    */
+
+
+    Game.prototype.drawFigure = function(coordinats,color){
 
         this.coordinats = coordinats;
+        this.color = color;
 
         var canvas = document.getElementById('canvas');
         var ctx = canvas.getContext('2d');
@@ -69,13 +119,13 @@ var Game = (function () {
             
             ctx.moveTo(coordinats[i][0][0],coordinats[i][0][1]);
             ctx.lineTo(coordinats[i][1][0],coordinats[i][1][1]);
+            ctx.strokeStyle = this.color;
             ctx.stroke();
 
         }
 
     }
 
-	   
 
     return Game;
 
