dbfafef819465c2833784877762460376f08a5fb
Murad Mikayilzadeh <m.murad@code.edu.az>
2017-05-10 21:49:55 +0400

    5/10/2017 21:49

--- mini.js
+++ mini.js
@@ -33,16 +33,48 @@ var Game = (function () {
 
     }
 
-   
+    //Bu funksiya oyun penceresine dordbucaqli cekir
+    Game.prototype.drawRect = function(a,b,c,d,bg){
+
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+        this.bg = bg;
+
+        var canvas = document.getElementById('canvas');
+        var ctx = canvas.getContext('2d');
+
+        ctx.fillStyle = this.bg;
+        ctx.fillRect(this.a,this.b,this.c,this.d);
+
+    }
+
+    //Bu funksiya ile koordinatlari array seklinde vermekle istenilen fiquru cekmek olar
+
+    Game.prototype.drawFigure = function(coordinats){
+
+        this.coordinats = coordinats;
+
+        var canvas = document.getElementById('canvas');
+        var ctx = canvas.getContext('2d');
+
+        for (var i = 0; i < coordinats.length; i++) {
+            
+            ctx.moveTo(coordinats[i][0][0],coordinats[i][0][1]);
+            ctx.lineTo(coordinats[i][1][0],coordinats[i][1][1]);
+            ctx.stroke();
+
+        }
+
+    }
+
+	   
 
     return Game;
 
 }());
 
-
-var oyun=new Game(400,400);
-
-oyun.changeWindowBg('#666666');
 
 
 
